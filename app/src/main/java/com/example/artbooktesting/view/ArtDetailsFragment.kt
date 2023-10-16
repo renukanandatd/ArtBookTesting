64e8432e59d8e622ce3c2f0a4272b737eb7d7181
renukanandatd <renukananda2000@gmail.com>
2023-10-16 11:06:56 +0530

    addded navigation

--- app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
@@ -2,12 +2,36 @@ package com.example.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.artbooktesting.R
+import com.example.artbooktesting.databinding.FragmentArtDetailsBinding
 
 class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
 
+    private var artDetailsBinding:FragmentArtDetailsBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtDetailsBinding.bind(view)
+        artDetailsBinding = binding
+
+        binding.imageView2.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+        val callback = object:OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
     }
-}
+
+    override fun onDestroyView() {
+        artDetailsBinding = null
+        super.onDestroyView()
+    }
+    }
--- app/src/main/java/com/example/artbooktesting/view/ArtFragment.kt
+++ app/src/main/java/com/example/artbooktesting/view/ArtFragment.kt
@@ -3,6 +3,7 @@ package com.example.artbooktesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.artbooktesting.R
 import com.example.artbooktesting.databinding.FragmentArtsBinding
 
@@ -12,5 +13,12 @@ class ArtFragment : Fragment(R.layout.fragment_arts) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtsBinding.bind(view)
+        artFragmentBinding = binding
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
     }
 }
