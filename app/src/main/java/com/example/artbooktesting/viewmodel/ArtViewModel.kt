ee64e99cec6e1c765bbef39baf12ce3afa162de6
renukanandatd <renukananda2000@gmail.com>
2023-10-19 15:11:26 +0530

    view model completed

--- app/src/main/java/com/example/artbooktesting/viewmodel/ArtViewModel.kt
+++ app/src/main/java/com/example/artbooktesting/viewmodel/ArtViewModel.kt
@@ -3,10 +3,12 @@ package com.example.artbooktesting.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.artbooktesting.model.ImageResponse
 import com.example.artbooktesting.repo.ArtRepositoryInterface
 import com.example.artbooktesting.roomdb.Art
 import com.example.artbooktesting.util.Resource
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ArtViewModel @Inject constructor(
@@ -34,5 +36,47 @@ class ArtViewModel @Inject constructor(
 
     fun resetInsertArtMsg(){
         insertArtMsg = MutableLiveData<Resource<Art>>()
+    }
+    fun setSelectedImage(url:String){
+        selectedImage.postValue(url)
+    }
+
+    fun deleteArt(art:Art) = viewModelScope.launch{
+        repository.deleteArt(art)
+    }
+
+    fun insertArt(art:Art) = viewModelScope.launch {
+        repository.insertArt(art)
+    }
+
+    fun makeArt(name:String, artistName : String, year :String){
+        if(name.isEmpty() || artistName.isEmpty() || year.isEmpty()){
+            insertArtMsg.postValue(Resource.error("Enter name, artist name, year",null))
+            return
+        }
+        val yearInt = try {
+            year.toInt()
+        }catch (e:Exception){
+            insertArtMsg.postValue(Resource.error("Year should be a number",null))
+            return
+        }
+
+        val art = Art(name,artistName,selectedImage.value?:"",yearInt)
+        insertArt(art)
+        setSelectedImage("")
+        insertArtMsg.postValue(Resource.success(art))
+    }
+
+    fun searchForImage(searchingString: String){
+
+        if (searchingString.isEmpty()){
+            return
+        }
+
+        images.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.searchImage(searchingString)
+            images.value = response
+        }
     }
 }
