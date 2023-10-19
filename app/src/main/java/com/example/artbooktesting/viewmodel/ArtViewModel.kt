37cf8e56fa8934daa8dfb1dc19695414d7434246
renukanandatd <renukananda2000@gmail.com>
2023-10-19 14:15:54 +0530

    view model added

--- app/src/main/java/com/example/artbooktesting/viewmodel/ArtViewModel.kt
+++ app/src/main/java/com/example/artbooktesting/viewmodel/ArtViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.artbooktesting.viewmodel
 
-class ArtViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.artbooktesting.model.ImageResponse
+import com.example.artbooktesting.repo.ArtRepositoryInterface
+import com.example.artbooktesting.roomdb.Art
+import com.example.artbooktesting.util.Resource
+import javax.inject.Inject
+
+class ArtViewModel @Inject constructor(
+    private val  repository : ArtRepositoryInterface
+) : ViewModel() {
+
+    //ART FRAGMENT
+    val artList = repository.getArt()
+
+
+    //IMAGE API FRAGMENT
+    private val images = MutableLiveData<Resource<ImageResponse>>()
+    val imageList : LiveData<Resource<ImageResponse>>
+        get() = images
+
+    private val selectedImage = MutableLiveData<String>()
+    val selectedImageURL : LiveData<String>
+        get() = selectedImage
+
+
+    //ART DETAILS FRAGMENT
+    private var insertArtMsg = MutableLiveData<Resource<Art>>()
+    val insertArtMessage : LiveData<Resource<Art>>
+        get() = insertArtMsg
+
+    fun resetInsertArtMsg(){
+        insertArtMsg = MutableLiveData<Resource<Art>>()
+    }
 }
