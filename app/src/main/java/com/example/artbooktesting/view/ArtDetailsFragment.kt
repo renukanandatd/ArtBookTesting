4a19807cf94e84ffeb3578efe625baac82a80a5d
renukanandatd <renukananda2000@gmail.com>
2023-10-20 16:16:59 +0530

    adapter classes  added

--- app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/example/artbooktesting/view/ArtDetailsFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.example.artbooktesting.R
 import com.example.artbooktesting.databinding.FragmentArtDetailsBinding
+import com.example.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtDetailsFragment@Inject constructor(
@@ -15,9 +17,12 @@ class ArtDetailsFragment@Inject constructor(
 )  : Fragment(R.layout.fragment_art_details) {
 
     private var artDetailsBinding:FragmentArtDetailsBinding? = null
+    lateinit var viewModel: ArtViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
 
         val binding = FragmentArtDetailsBinding.bind(view)
         artDetailsBinding = binding
--- app/src/main/java/com/example/artbooktesting/view/Image_Api_Fragment.kt
+++ app/src/main/java/com/example/artbooktesting/view/Image_Api_Fragment.kt
@@ -5,10 +5,9 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.artbooktesting.R
 import com.example.artbooktesting.adapter.ArtRecyclerAdapter
-import com.example.artbooktesting.adapter.ImageRecyclerAdapter
 import javax.inject.Inject
 
-class Image_Api_Fragment @Inject constructor(imageRecyclerAdapter: ImageRecyclerAdapter) : Fragment(R.layout.fragment_api) {
+class Image_Api_Fragment @Inject constructor(imageRecyclerAdapter: ArtRecyclerAdapter) : Fragment(R.layout.fragment_api) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
